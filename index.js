45141f272fc6a9a261d0f1ff7f0bdfa6c40d99f0
gustavo <gtva5446@gmail.com>
2024-11-05 10:44:34 -0300

    fix coding

--- index.js
+++ index.js
@@ -1,89 +1,86 @@
-const itemInput = document.getElementById("input");
+const itemInput = document.getElementById("input")
 
 function loadInitialState() {
-  const state = localStorage.getItem("state");
-  const list = JSON.parse(state) ?? [];
-  list.forEach((element) => {
-    addItemToUI(element);
-  });
+    const state = localStorage.getItem("state")
+    const list = JSON.parse(state) ?? []
+    list.forEach((element) => {
+        addItemToUI(element)
+    })
 }
 
 function initializeListeners() {
-  itemInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      addItemFromInput();
-    }
-  });
+    itemInput.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            addItemFromInput()
+        }
+    })
 
-  document
-    .getElementById("add-item-btn")
-    .addEventListener("click", addItemFromInput);
+    const closeNotificationButton = document.querySelector('.delete')
 
-  document
-    .getElementById("save-btn")
-    .addEventListener("click", saveAllItemsToLocalStorage);
+    closeNotificationButton.addEventListener('click', () => {
+        closeNotificationButton.parentElement.classList.add('is-hidden')
+    })
 
-  document.getElementById("clear-btn").addEventListener("click", clearAll);
+    document
+        .getElementById("add-item-btn")
+        .addEventListener("click", addItemFromInput)
+
+    document
+        .getElementById("save-btn")
+        .addEventListener("click", saveAllItemsToLocalStorage)
+
+    document.getElementById("clear-btn").addEventListener("click", clearAll)
 }
 
 function saveAllItemsToLocalStorage() {
-  const listHTML = document.getElementById("list");
-  const listItems = Array.from(listHTML.children).map(function (el) {
-    const label = el.innerText;
-    const checkbox = el.firstChild.checked;
-    return {
-      label: label,
-      isChecked: checkbox,
-    };
-  });
-  localStorage.setItem("state", JSON.stringify(listItems));
-//   alert("saved succcesfully");
+    const listHTML = document.getElementById("list")
+    const listItems = Array.from(listHTML.children).map(function (el) {
+        const label = el.innerText
+        const checkbox = el.firstChild.checked
+        return {
+            label: label,
+            isChecked: checkbox,
+        }
+    })
+    localStorage.setItem("state", JSON.stringify(listItems))
+    //   alert("saved succcesfully")
+    document.querySelector('.notification').classList.remove('is-hidden')
 }
 
 function addItemToUI({ label, isChecked }) {
-  const listItem = document.createElement("li");
-  document.getElementById("list").appendChild(listItem);
+    const listItem = document.createElement("li")
+    document.getElementById("list").appendChild(listItem)
 
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.checked = isChecked;
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    checkbox.checked = isChecked
 
-  listItem.append(checkbox, label);
+    listItem.append(checkbox, label)
 
-  setCheckboxClasses({ checkbox, listItem });
+    setCheckboxClasses({ checkbox, listItem })
 
-  checkbox.addEventListener("change", () => {
-    setCheckboxClasses({ checkbox, listItem });
-  });
+    checkbox.addEventListener("change", () => {
+        setCheckboxClasses({ checkbox, listItem })
+    })
 }
 
 function setCheckboxClasses({ checkbox, listItem }) {
-  if (checkbox.checked) {
-    listItem.classList.add("checked");
-  } else {
-    listItem.classList.remove("checked");
-  }
+    if (checkbox.checked) {
+        listItem.classList.add("checked")
+    } else {
+        listItem.classList.remove("checked")
+    }
 }
 
 function addItemFromInput() {
-  addItemToUI({ label: itemInput.value, isChecked: false });
-  itemInput.value = "";
+    addItemToUI({ label: itemInput.value, isChecked: false })
+    itemInput.value = ""
 }
 
 function clearAll() {
-  localStorage.clear();
-  document.getElementById("list").replaceChildren("");
+    localStorage.clear()
+    document.getElementById("list").replaceChildren("")
 }
 
-loadInitialState();
-initializeListeners();
-
-const closeNotificationButton = document.querySelector('.delete')
-
-closeNotificationButton.addEventListener('click', () => {
-    closeNotificationButton.parentElement.classList.add('is-hidden')
-})
-
-document.getElementById("save-btn").addEventListener("click", () => {
-    document.querySelector('.notification').classList.remove('is-hidden')
-})
+loadInitialState()
+initializeListeners()
